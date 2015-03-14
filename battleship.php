8f00d14ebd3b3c26b9f297a40821c35d61e5c5d5
zmcalexander <zmcalexander@gmail.com>
2015-03-13 23:49:14 -0400

    Update battleship.php

--- battleship.php
+++ battleship.php
@@ -1,7 +1,22 @@
 <?php
 
 
-// this is a comment
+function buildGridArray() {
+	
+// build the column arrays
+$column1 = array(array(" O ", "A1"), array(" O ", "A2"), array(" O ", "A3"), array(" O ", "A4"), array(" O ", "A5"), array(" O ", "A6"), array(" O ", "A7"), array(" O ", "A8"), array(" O ", "A9"), array(" O ", "A10")); 
+$column2 = array(array(" O ", "B1"), array(" O ", "B2"), array(" O ", "B3"), array(" O ", "B4"), array(" O ", "B5"), array(" O ", "B6"), array(" O ", "B7"), array(" O ", "B8"), array(" O ", "B9"), array(" O ", "B10")); 
+$column3 = array(array(" O ", "C1"), array(" O ", "C2"), array(" O ", "C3"), array(" O ", "C4"), array(" O ", "C5"), array(" O ", "C6"), array(" O ", "C7"), array(" O ", "C8"), array(" O ", "C9"), array(" O ", "C10")); 
+$column4 = array(array(" O ", "D1"), array(" O ", "D2"), array(" O ", "D3"), array(" O ", "D4"), array(" O ", "D5"), array(" O ", "D6"), array(" O ", "D7"), array(" O ", "D8"), array(" O ", "D9"), array(" O ", "D10")); 
+$column5 = array(array(" O ", "E1"), array(" O ", "E2"), array(" O ", "E3"), array(" O ", "E4"), array(" O ", "E5"), array(" O ", "E6"), array(" O ", "E7"), array(" O ", "E8"), array(" O ", "E9"), array(" O ", "E10")); 
+$column6 = array(array(" O ", "F1"), array(" O ", "F2"), array(" O ", "F3"), array(" O ", "F4"), array(" O ", "F5"), array(" O ", "F6"), array(" O ", "F7"), array(" O ", "F8"), array(" O ", "F9"), array(" O ", "F10")); 
+$column7 = array(array(" O ", "G1"), array(" O ", "G2"), array(" O ", "G3"), array(" O ", "G4"), array(" O ", "G5"), array(" O ", "G6"), array(" O ", "G7"), array(" O ", "G8"), array(" O ", "G9"), array(" O ", "G10")); 
+$column8 = array(array(" O ", "H1"), array(" O ", "H2"), array(" O ", "H3"), array(" O ", "H4"), array(" O ", "H5"), array(" O ", "H6"), array(" O ", "H7"), array(" O ", "H8"), array(" O ", "H9"), array(" O ", "H10")); 
+$column9 = array(array(" O ", "I1"), array(" O ", "I2"), array(" O ", "I3"), array(" O ", "I4"), array(" O ", "I5"), array(" O ", "I6"), array(" O ", "I7"), array(" O ", "I8"), array(" O ", "I9"), array(" O ", "I10")); 
+$column10 = array(array(" O ", "J1"), array(" O ", "J2"), array(" O ", "J3"), array(" O ", "J4"), array(" O ", "J5"), array(" O ", "J6"), array(" O ", "J7"), array(" O ", "J8"), array(" O ", "J9"), array(" O ", "J10")); 
+
+
+}
 
 
 
